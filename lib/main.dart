b005ab228664e8bdfc2481377210943b3c3ca839
Gil <quickerz59@gmail.com>
2020-09-04 15:09:32 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import './random_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.red[400],
+      ),
+      home: RandomWords(),
+    );
+  }
+}
